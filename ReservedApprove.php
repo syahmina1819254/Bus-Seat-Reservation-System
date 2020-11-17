e20de180dc235b5f420cac8e06e77c9af56dfcac
syahmina1819254 <55779851+syahmina1819254@users.noreply.github.com>
2020-11-18 01:53:50 +0800

    Update ReservedApprove.php
    add price

--- ReservedApprove.php
+++ ReservedApprove.php
@@ -18,6 +18,17 @@
     $age= $_POST['age'];
     $Email= $_POST['Email'];
     $contact= $_POST['Contact'];
+
+    function Price($seat)
+    {
+      if ($seat==1||$seat==4||$seat==7||$seat==10||
+      $seat==13||$seat==16||$seat==19||$seat==22||$seat==25
+      ||$seat==28 ) { echo "Single Seat";
+      }
+      else {
+        echo "RM30(Double Seat)";
+      }
+    }
      ?>
 
       <div class="banner">
@@ -42,6 +53,7 @@
           <p>Arrival: <?php  echo $_SESSION['details']['arrival'];?></p>
           <p>Date: <?php echo $_SESSION['details']['date']; ?></p>
           <p>Seat number: <?php echo $Seatreserve ?></p>
+          <p>Paid:  <?php Price($Seatreserve); ?></p>
 
 
 
