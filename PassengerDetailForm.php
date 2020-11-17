be7b6965a6f41bd799c71b3a0c468dbfe9bd0697
syahmina1819254 <55779851+syahmina1819254@users.noreply.github.com>
2020-11-17 22:35:21 +0800

    Update and rename PassengerDetailForm.html to PassengerDetailForm.php

--- PassengerDetailForm.php
+++ PassengerDetailForm.php
@@ -8,21 +8,30 @@
 
   </head>
   <body>
+<?php
+session_start();
+$Seatreserve =  $_POST['seat'];
 
+ ?>
     <div class="banner">
       <h1>Bus Seat Reservation</h1>
+      
     </div>
         <div class="Formbox">
-          <form action="/">
+          <form action="ReservedApprove.php" method="post">
         <div >
         <h2>Passenger Information</h2>
         </div>
 
         <div class="item">
+          <div class="name-item">
+              Seat Number: <?php echo $Seatreserve; ?>
+               <input type="hidden" id="custId" name="custSeat" value="<?php echo $Seatreserve; ?>">
+          </div>
           <p>Name</p>
           <div class="name-item">
-            <input type="text" name="name" placeholder="First" />
-            <input type="text" name="name" placeholder="Last" />
+            <input type="text" name="Fname" placeholder="First" />
+            <input type="text" name="Lname" placeholder="Last" />
           </div>
         </div>
         <div class="item">
@@ -35,11 +44,11 @@
         </div>
         <div class="item">
           <p>Email</p>
-          <input type="text" name="name"/>
+          <input type="text" name="Email"/>
         </div>
         <div class="item">
           <p>Phone</p>
-          <input type="text" name="name"/>
+          <input type="text" name="Contact"/>
         </div>
         <div class="btn-block">
                  <button type="submit">SEND</button>
